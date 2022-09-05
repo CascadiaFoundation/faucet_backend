75b09ac04e38a96dc5eaca6dd50b7e6d7846f996
phantom930 <phantom93930@gmail.com>
2022-09-05 21:40:49 +0900

    feat:shell-script-validation

--- src/apis/sendFaucetRequest.js
+++ src/apis/sendFaucetRequest.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const shell = require("shelljs");
 
 const sendFaucetRequest = async (address) => {
-    shell.exec(`${process.env.SHELL_SCRIPT} ${address}`);
+    const res = await shell.exec(`${process.env.SHELL_SCRIPT} ${address}`);
+
+    if (res.stderr) {
+        throw "Invalid shell script.";
+    }
 
     return;
 };
