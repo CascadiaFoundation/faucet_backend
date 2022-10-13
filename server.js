08993f280080d6b640e0afec867bec781bad36fd
phantom930 <phantom93930@gmail.com>
2022-10-13 22:27:57 +0900

    feat:limit-middleware

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const app = require("./src/app");
 const config = require("./src/config");
 
-mongoose
-    .connect(config.mongoose.url)
-    .then(() => {
-        console.log("Connected to MongoDB");
-        app.listen(config.port, () => {
-            console.log(`Listening on port ${config.port}`);
-        });
-    })
-    .catch((err) => console.log(err));
+app.listen(config.port, () => {
+    console.log(`Listening on port ${config.port}`);
+});
+// mongoose
+//     .connect(config.mongoose.url)
+//     .then(() => {
+//         console.log("Connected to MongoDB");
+//     })
+//     .catch((err) => console.log(err));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 
 const { getFaucet } = require("../controllers");
 const { checkAddress } = require("../middleware");
 
 const router = express.Router();
 
-router.post("/get-faucet", checkAddress, getFaucet);
+const limiter = rateLimit({
+    max: 1,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many request from this IP"
+});
+
+router.post("/get-faucet", [checkAddress, limiter], getFaucet);
 
 module.exports = router;
