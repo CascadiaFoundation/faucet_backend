071510f1f70f8dc63cf4267cf051d16eb59a3ee9
phantom930 <phantom93930@gmail.com>
2022-10-14 02:12:35 +0900

    feat:web3-faucet-transfer

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,9 @@ const { sendFaucetRequest } = require("../apis");
 
 const getFaucet = async (req, res) => {
     try {
-        await sendFaucetRequest(req.body.address);
+        sendFaucetRequest(req.body.address).then((res) =>{
+            console.log(res);
+        });
         res.send("Test coin has been achieved.");
     } catch (err) {
         res.status(500).send(err);
