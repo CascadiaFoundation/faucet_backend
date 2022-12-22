9bee850d562e9675b862e2c5eb6c45222873efed
Dev_Star <phantom93930@gmail.com>
2022-12-22 12:03:08 -0500

    fix: remove-email-postgre-implement

--- src/model/cascadia.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Cascadia = sequelize.define("cascadia", {
-        email: {
-            type: Sequelize.STRING
-        },
-    });
-
-    return Cascadia;
-};
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const rateLimit = require("express-rate-limit");
 
 const config = require("../config");
 const { getFaucet } = require("../controllers");
-const { addEmail } = require("../controllers/addEmail");
 const { addResponse } = require("../controllers/addResponse");
 const { checkAddress } = require("../middleware");
 
@@ -12,11 +11,10 @@ const router = express.Router();
 const limiter = rateLimit({
     max: 1,
     windowMs: config.limitDuration,
-    message: "Too many request from this IP address."
+    message: "Too many request from this IP address.",
 });
 
 router.post("/get-faucet", [checkAddress, limiter], getFaucet);
-router.post("/add-email", addEmail);
 router.post("/add-response", addResponse);
 
 module.exports = router;
